73aeb1d24a4cbaee4a1b626353abc797d5b971a1
Sander Marechal <s.marechal@jejik.com>
2013-04-13 12:24:29 +0200

    Add support for -f and -a options

--- PHPCtags.class.php
+++ PHPCtags.class.php
@@ -223,11 +223,16 @@ class PHPCtags
         return $str;
     }
 
-    public function export($file, $options)
+    public function get($file, $options)
     {
         //@todo Check for existence
         $this->mFile = $file;
         $structs = $this->struct($this->mParser->parse(file_get_contents($this->mFile)), TRUE);
-        echo $this->render($structs, $options);
+        return $this->render($structs, $options);
+    }
+
+    public function export($file, $options)
+    {
+        echo $this->get($file, $options);
     }
 }
